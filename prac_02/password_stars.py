02dabb4b4a8e005bcb74aea1d5f55d816bd03018
jd113272 <cameron.colless@my.jcu.edu.au>
2025-09-24 14:54:40 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,22 @@
-"""
-convert password to asterisks
-"""
-password = input("Please enter password: ")
-minimum_length = 5
+"""convert password to asterisks"""
 
-while len(password) < minimum_length:
+
+def main():
+    minimum_length = 5
+    password = get_password(minimum_length)
+    print_asterisks(password)
+
+
+def print_asterisks(password: str):
+    print("*" * len(password))
+
+
+def get_password(minimum_length: int) -> str:
     password = input("Please enter password: ")
-print("*" * len(password))
+
+    while len(password) < minimum_length:
+        password = input("Please enter password: ")
+    return password
+
+
+main()
