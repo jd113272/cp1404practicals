7d3930df9867bce09156a9897e0986df4a2c3fcc
jd113272 <cameron.colless@my.jcu.edu.au>
2025-10-01 14:30:55 +1000

    Print random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,15 @@
+import random
+
+# What did you see on line 1?
+# Random numbers between 5 and 20.
+# Smallest number that could be outputted was 5, and the largest was 20
+
+# What did you see on line 2?
+# Random numbers between 3 and 10, in intervals of 2 (i.e. 3, 5, 7 & 9)
+# This line could not produce a 4 as this is only one number away from 3, not a multiple of 2.
+
+# What did you see on line 3?
+# Random float numbers between 2.5 and 5.5
+# Smallest number that could be outputted was 2.5, and the largest number that could be outputted was 5.5
+
+print(random.randint(1, 100))
