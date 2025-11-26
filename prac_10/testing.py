766d02a25cc3f9ccee4b296731ac6b157b22e8f7
jd113272 <cameron.colless@my.jcu.edu.au>
2025-11-26 13:31:10 +1000

    Complete Prac 10 testing task

---
+++ prac_10/testing.py
@@ -0,0 +1,84 @@
+"""
+CP1404/CP5632 Practical
+Testing code using assert and doctest
+"""
+
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return " ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
+
+def run_tests():
+    """Run the tests on the functions."""
+    # assert test with no message - used to see if the function works properly
+    assert repeat_string("Python", 1) == "Python"
+    # the test below should fail
+    assert repeat_string("hi", 2) == "hi hi"
+
+    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
+
+    # assert test with custom message,
+    # used to see if Car's init method sets the odometer correctly
+    # this should pass (no output)
+    car = Car()
+    assert car._odometer == 0, "Car does not set odometer correctly"
+
+    # Note that Car's __init__ function sets the fuel in one of two ways:
+    # using the value passed in or the default
+    # You should test both of these
+    car = Car(fuel=10)
+    assert car.fuel == 10
+    second_car = Car()
+    assert second_car.fuel == 0
+
+
+def convert_phrase_to_sentence(phrase):
+    """
+    Convert the given phrase to a sentence with a capital letter at the start and a full stop at the end.
+    >>> convert_phrase_to_sentence("hello")
+    'Hello.'
+    >>> convert_phrase_to_sentence("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> convert_phrase_to_sentence("this is a phrase.")
+    'This is a phrase.'
+    """
+    if phrase[-1] == ".":
+        end_character = ""
+    else:
+        end_character = "."
+    return f"{phrase[0].upper()}{phrase[1:]}{end_character}"
+
+
+run_tests()
+
+# (PyCharm may see your >>> doctest comments and run doctests anyway.)
+doctest.testmod()
+
+# (Don't change the tests, change the function!)
+
+# TODO: 5. Write and test a function to format a phrase as a sentence,
+# starting with a capital and ending with a single full stop.
+# Important: start with a function header and just use pass as the body
+# then add doctests for 3 tests:
+#   'hello' -> 'Hello.'
+#   'It is an ex parrot.' -> 'It is an ex parrot.'
+# and one more that you decide is a useful test.
+# Run your doctests and watch the tests fail.
+# Then write the body of the function so that the tests pass.
